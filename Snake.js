622de51e4c5f6bda2219626e9757e878223cabb8
ModyH1904 <62265426+ModyH1904@users.noreply.github.com>
2022-02-11 12:24:24 +0300

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -10,11 +10,11 @@ let gameOver = false;
 function main(currentTime){
     if (gameOver){
         if(confirm('You lost. Press Ok to reset')){
-            window.location = './index.html';
+            window.location = './snake.html';
         }
-    }
         return
     }
+
 
 
 
